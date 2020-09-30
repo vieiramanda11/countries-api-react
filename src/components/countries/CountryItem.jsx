a78d0ad18cc16f192cee79c7c96902fa38daf9d3
Amanda <amandavieirazevedo@gmail.com>
2020-09-30 19:51:18 -0300

    changing class components to functional components

--- src/components/countries/CountryItem.jsx
+++ src/components/countries/CountryItem.jsx
@@ -1,19 +1,21 @@
-import React, { Component } from 'react'
+import React from "react";
+import PropTypes from 'prop-types';
 
-class CountryItem extends Component {
-    render() {
-        const { id, flag, name, population, region, capital } = this.props.country;
-        return (
-            <div>
-                <h5>ID: {id}</h5>
-                <h5>Flag: {flag}</h5>
-                <h5>Name: {name}</h5>
-                <h5>Population: {population}</h5>
-                <h5>Region: {region}</h5>
-                <h5>Capital: {capital}</h5>
-            </div>
-        )
-    }
+const CountryItem = ({ country: { id, flag, name, population, region, capital }}) => {
+  return (
+    <div>
+      <h5>ID: {id}</h5>
+      <h5>Flag: {flag}</h5>
+      <h5>Name: {name}</h5>
+      <h5>Population: {population}</h5>
+      <h5>Region: {region}</h5>
+      <h5>Capital: {capital}</h5>
+    </div>
+  );
+};
+
+CountryItem.propTypes = {
+    country: PropTypes.object.isRequired,
 }
 
-export default CountryItem
+export default CountryItem;
