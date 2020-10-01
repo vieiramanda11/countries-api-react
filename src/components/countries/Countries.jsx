164fdd5dd0984f426c062a2c12b970c9ef498c32
Amanda <amandavieirazevedo@gmail.com>
2020-10-01 11:35:44 -0300

    creating spinning component

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,16 +1,25 @@
-import React, { Component } from "react";
+import React from "react";
 import CountryItem from "./CountryItem";
+import Spinner from "../layout/Spinner";
+import PropTypes from "prop-types";
 
-class Countries extends Component {
-  render() {
+const Countries = ({ countries, loading }) => {
+  if (loading) {
+    return <Spinner />;
+  } else {
     return (
       <div>
-        {this.props.countries.map((country) => (
+        {countries.map((country) => (
           <CountryItem key={country.alpha3Code} country={country} />
         ))}
       </div>
     );
   }
+};
+
+Countries.propTypes = {
+  countries: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
 }
 
 export default Countries;
--- src/components/countries/CountryItem.jsx
+++ src/components/countries/CountryItem.jsx
@@ -1,22 +1,27 @@
 import React from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-const CountryItem = ({ country: { alpha3Code, name, population, region, capital }}) => {
-  const countryCode = alpha3Code.toLowerCase(); 
+const CountryItem = ({
+  country: { alpha3Code, name, population, region, capital },
+}) => {
+  const countryCode = alpha3Code.toLowerCase();
   return (
     <div>
-      <h5>ID: {alpha3Code}</h5>
+      <img
+        src={`https://restcountries.eu/data/${countryCode}.svg`}
+        style={{ width: "100px", height: "50px" }}
+        alt="country flag"
+      />
       <h5>Name: {name}</h5>
       <h5>Population: {population}</h5>
       <h5>Region: {region}</h5>
       <h5>Capital: {capital}</h5>
-      <img src={`https://restcountries.eu/data/${countryCode}.svg`} style={{width: "100px", height: "50px"}} alt="country flag"/>
     </div>
   );
 };
 
 CountryItem.propTypes = {
-    country: PropTypes.object.isRequired,
-}
+  country: PropTypes.object.isRequired,
+};
 
 export default CountryItem;
